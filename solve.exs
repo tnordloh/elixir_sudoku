2f0bfb21bdd43159c7f1618828fe4760b6b9afd4
Timothy Nordloh <tnordloh@gmail.com>
2017-03-23 11:38:37 -0600

    add comment on solved function

--- solve.exs
+++ solve.exs
@@ -19,7 +19,7 @@ defmodule Solver do
   end
   def solve(list, solved) do
     IO.puts "solution found" if solved solved
-    #The solved variable is just to complete this crappy while loop.
+    #The solved variable is just to break my pseudo while loop.
     #I used the 'puts' to kill the 'unused variable' warning.
     IO.puts Sudoku.to_s(list)
   end
